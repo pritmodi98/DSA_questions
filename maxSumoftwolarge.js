c64640817846d89674ad6cf26646fe48f0c2aa9c
Prit_Modi <pritmodi98@gmail.com>
2022-06-26 17:40:46 +0530

    maxSum

--- maxSumoftwolarge.js
+++ maxSumoftwolarge.js
@@ -4,8 +4,17 @@ function maxSum(arr) {
         first=arr[1]
         second=arr[0]
     }
-    
-
+    for (let i = 2; i < arr.length; i++) {
+        if (arr[i]>first) {
+            second=first
+            first=arr[i]
+        }
+        else if (arr[i]>second && arr[i]!==first) {
+            second=arr[i]
+        }
+        
+    }
+    return first+second
 }
 
-console.log(maxSum([12,4,67,2,34,81]))
+console.log(maxSum([12,4,67,2,34,67]))
