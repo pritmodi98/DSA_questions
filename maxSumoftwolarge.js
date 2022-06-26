41b1e279c2457836fe18860b8a49c369612335d7
Prit_Modi <pritmodi98@gmail.com>
2022-06-26 17:15:59 +0530

    remove anagrams

--- maxSumoftwolarge.js
+++ maxSumoftwolarge.js
@@ -1,5 +1,11 @@
 function maxSum(arr) {
+    let first=arr[0],second=arr[1]
+    if (second>first) {
+        first=arr[1]
+        second=arr[0]
+    }
     
+
 }
 
 console.log(maxSum([12,4,67,2,34,81]))
